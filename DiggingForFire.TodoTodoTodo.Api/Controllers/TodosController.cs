b4d310cc90762894cef49fc78ed92a8a4c295aab
Robbie Luijben <diggingforfire@gmail.com>
2019-05-30 19:03:30 +0200

    Initial setup

---
+++ DiggingForFire.TodoTodoTodo.Api/Controllers/TodosController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DiggingForFire.TodoTodoTodo.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodosController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            await Task.Delay(20);
+
+            return new OkObjectResult("1");
+        }
+    }
+}
